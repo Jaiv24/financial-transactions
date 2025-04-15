11bd3baa2f2bab8399bee8ef72e5ca601dfd1910
jaiv24 <jaiv.maurya@gmail.com>
2025-04-14 22:38:27 -0600

    Refactor CSS styles for create and edit forms; update transaction data in db.json

--- app/admin/edit/[id]/actions.js
+++ app/admin/edit/[id]/actions.js
@@ -1,15 +1,15 @@
-'use server';
+"use server";
 
-import { revalidatePath } from 'next/cache';
+import { revalidatePath } from "next/cache";
 
 export async function updateTransaction(id, data) {
   await fetch(`http://localhost:4000/transactions/${id}`, {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   });
-  revalidatePath('/collection');
+  revalidatePath("/collection");
   revalidatePath(`/collection/${id}`);
-  revalidatePath('/admin');
+  revalidatePath("/admin");
   revalidatePath(`/admin/edit/${id}`);
 }
